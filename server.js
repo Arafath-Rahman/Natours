4f7843f5d0c6d5493290a31917e16637ebbc43c7
Md. Arafath Rahman Khan <mkhan172093@bscse.uiu.ac.bd>
2020-06-26 15:09:35 +0600

    handled 'uncaugt exceotions & unhandled errors'

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNHANDLED EXCEPTION! SHUTTING DOWN....');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-console.log(process.env.NODE_ENV);
+//console.log(process.env.NODE_ENV);
 
 const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
 mongoose
@@ -21,6 +27,14 @@ mongoose
 
 //START SERVER 🚉
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Listening to port ${port}..`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! SHUTTING DOWN....');
+  server.close(() => {
+    process.exit(1);
+  });
 });

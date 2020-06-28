941ed7242cf8d2ac43b85ff0457b125d6032a639
Md. Arafath Rahman Khan <mkhan172093@bscse.uiu.ac.bd>
2020-06-29 01:40:09 +0600

    implemented 'updating current user data'

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,45 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const filterObj = (obj, ...alllowedFields) => {
+  const newObj = {};
+
+  Object.keys(obj).forEach((el) => {
+    if (alllowedFields.includes(el)) newObj[el] = obj[el];
+  });
+
+  return newObj;
+};
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  //create an error if user tries to update password
+  if (req.body.password || req.body.confirmPassword) {
+    return next(
+      new AppError(
+        'This route is not for update password. Please use /updateMyPassword route',
+        400
+      )
+    );
+  }
+
+  //2) filtered out unwanted properties which should not be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  console.log(filteredBody);
+  //3)update the informations
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+});
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();

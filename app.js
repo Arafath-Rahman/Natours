289a7e52d915fa07f05f789514972dcc38c827ba
Md. Arafath Rahman Khan <mkhan172093@bscse.uiu.ac.bd>
2020-06-29 22:37:14 +0600

    preventing NoSQL query injection & XSS

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -30,6 +32,12 @@ app.use('/api', limiter);
 
 // body parser, reading data from body to req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data Sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data Sanitization against XSS
+app.use(xss());
 
 //using 'static' build-in middleware to serve static files from folders
 app.use(express.static(`${__dirname}/public`));

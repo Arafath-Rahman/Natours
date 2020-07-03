211569338b0f90feb1710defc2033c06ee1d3bfc
Md. Arafath Rahman Khan <mkhan172093@bscse.uiu.ac.bd>
2020-07-03 20:54:14 +0600

    implemented nested routes with express

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
+const reviewRouter = require('../routes/reviewRoutes');
 
 const router = express.Router();
 
 //router.param('id', tourController.checkID);
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap-tours')
@@ -27,5 +30,17 @@ router
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour
   );
+
+//POST /tour/:tourId/reviews
+//GET /tour/:tourId/reviews
+//GET tour/:tourId/reviews/:reviewId
+
+// router
+//   .route('/:tourId/reviews')
+//   .post(
+//     authController.protect,
+//     authController.restrictTo('user'),
+//     reviewController.createReview
+//   );
 
 module.exports = router;

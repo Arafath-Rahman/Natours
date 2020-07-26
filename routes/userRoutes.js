2d3a4617bfd8956c53fe6192022c5e41d07d892b
Md. Arafath Rahman Khan <mkhan172093@bscse.uiu.ac.bd>
2020-07-26 07:39:09 +0600

    updated '/me' route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,13 +15,21 @@ router.patch(
   authController.updatePassword
 );
 
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
-
 router
   .route('/')
   .get(userController.getAllUsers)
   .post(userController.createUser);
+
+router.get(
+  '/me',
+  authController.protect,
+  userController.getMe,
+  userController.getUser
+);
+
+router.patch('/updateMe', authController.protect, userController.updateMe);
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
+
 router
   .route('/:id')
   .get(userController.getUser)
